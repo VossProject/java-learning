b9b765f42e4446abec5c253d1b83fe09e7a9bedc
Mikey Voss <vossproject@icloud.com>
2026-02-05 22:15:09 -0800

    feat: add primitive type and string examples

--- 01-fundamentals/variables-and-types/VariablesDemo.java
+++ 01-fundamentals/variables-and-types/VariablesDemo.java
@@ -9,7 +9,24 @@
  */
 public class VariablesDemo {
     public static void main(String[] args) {
-        // TODO: Start experimenting here!
-        System.out.println("Hello, Java!");
+        // --- Integer types ---
+        int age = 25;
+        long worldPopulation = 8_000_000_000L;
+
+        System.out.println("Age: " + age);
+        System.out.println("World population: " + worldPopulation);
+
+        // --- Floating point ---
+        double pi = 3.14159;
+        System.out.println("Pi: " + pi);
+
+        // --- Boolean ---
+        boolean isLearning = true;
+        System.out.println("Currently learning Java: " + isLearning);
+
+        // --- Char and String ---
+        char grade = 'A';
+        String name = "Mikey";
+        System.out.println(name + " got a grade of " + grade);
     }
 }
