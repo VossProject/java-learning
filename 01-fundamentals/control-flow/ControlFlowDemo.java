2923b423ff8380d005ba1ff52363f1e2c9e154ad
Mikey Voss <vossproject@icloud.com>
2026-02-09 21:37:54 -0800

    feat: add grade calculator using if/else chains

--- 01-fundamentals/control-flow/ControlFlowDemo.java
+++ 01-fundamentals/control-flow/ControlFlowDemo.java
@@ -10,7 +10,24 @@
  */
 public class ControlFlowDemo {
     public static void main(String[] args) {
-        // TODO: Start experimenting here!
-        System.out.println("Control Flow");
+        // score var
+        int score = 90;
+
+        // print grade
+        if (score >= 90) {
+            System.out.println("A");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
     }
 }
