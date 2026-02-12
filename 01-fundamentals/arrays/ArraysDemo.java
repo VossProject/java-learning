c505bb3c31118d28fd48e76520783e36304d564d
Mikey Voss <vossproject@icloud.com>
2026-02-12 11:36:47 -0800

    feat: add array basics

--- 01-fundamentals/arrays/ArraysDemo.java
+++ 01-fundamentals/arrays/ArraysDemo.java
@@ -10,7 +10,17 @@
  */
 public class ArraysDemo {
     public static void main(String[] args) {
-        // TODO: Start experimenting here!
-        System.out.println("Arrays");
+        // Array Basics
+        // Create array of int called scores: 85, 92, 78, 95, 88
+        int[] scores = {85, 92, 78, 95, 88};
+        // Print first and last elements
+        System.out.println("First element: " + scores[0]);
+        System.out.println("Last element: " + scores[scores.length-1]);
+        // Change third element to 100
+        scores[2] = 100;
+        // Use for loop print every element: ie; Score 1: 85
+        for (int i = 1; i <= scores.length; i++) {
+            System.out.println("Score " + i + ": " + scores[i-1]);
+        }
     }
 }
